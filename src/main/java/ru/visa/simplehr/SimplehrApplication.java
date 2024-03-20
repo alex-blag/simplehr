e4753e0031ad3f6a9398dc89c1b78a5b54b0c7ec
Ab <104671142+alex-blag@users.noreply.github.com>
2024-03-20 12:01:11 +0200

    feat: application skeleton

---
+++ src/main/java/ru/visa/simplehr/SimplehrApplication.java
@@ -0,0 +1,13 @@
+package ru.visa.simplehr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplehrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplehrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/visa/simplehr/SimplehrApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.visa.simplehr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplehrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
